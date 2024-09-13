ec56b9c0c212465d9884dc563641998c4be1ae5f
Ksrind1208 <minhducm1208@gmail.com>
2024-09-13 22:43:56 +0700

    Change some texts

--- SPI/SPI_ESP32/Core/Src/main.c
+++ SPI/SPI_ESP32/Core/Src/main.c
@@ -61,17 +61,19 @@ static void MX_SPI1_Init(void);
 /* USER CODE BEGIN 0 */
 uint8_t rxBuffer[1]={0};
 uint8_t rxBuffer_Slave[30];
-uint8_t txBuffer_Slave[12]="from slave";
+uint8_t txBuffer_Slave[30]="from slave";
 uint8_t txBuffer[1]={0};
 uint8_t index=0;
 void HAL_SPI_RxCpltCallback(SPI_HandleTypeDef *hspi){
 	if(hspi->Instance==SPI1){
-
+		txBuffer[0]=txBuffer_Slave[index];
+		HAL_SPI_Transmit(&hspi1,txBuffer,1,100);
 		HAL_SPI_Receive_IT(&hspi1,rxBuffer,1);	
 
 		if(rxBuffer[0]=='q'){
 			index=0;
 		}else{
+
 			rxBuffer_Slave[index]=rxBuffer[0];
 			if(index>30 || index==30 ){
 				index=0;
@@ -115,7 +117,7 @@ int main(void)
   MX_DMA_Init();
   MX_SPI1_Init();
   /* USER CODE BEGIN 2 */
-	
+
 	HAL_SPI_Receive_IT(&hspi1,rxBuffer,1);
 	
   /* USER CODE END 2 */
@@ -143,10 +145,13 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -156,12 +161,12 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     Error_Handler();
   }
@@ -234,6 +239,7 @@ static void MX_GPIO_Init(void)
 /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
